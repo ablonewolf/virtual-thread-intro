5d100547cc239ba208f305f96aa6f6e4c2fa30d2
Arka Bhuiyan <belmont66@duck.com>
2024-12-10 15:26:38 +0600

    demonstrated how to create virtual threads and how to keep them waiting to simulate a blocking scenario, reduced the number of max platform threads, used a logger to log statements when needed.

--- src/main/java/org/ablonewolf/basic/InboundOutboundTaskDemo.java
+++ src/main/java/org/ablonewolf/basic/InboundOutboundTaskDemo.java
@@ -1,11 +1,19 @@
 package org.ablonewolf.basic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.CountDownLatch;
+
 public class InboundOutboundTaskDemo {
 
-    private static final Integer MAX_PLATFORM_THREADS = 60000;
+    private static final Integer MAX_PLATFORM_THREADS = 200;
+    private static final Integer MAX_VIRTUAL_THREADS = 50000;
+    private static final Logger logger = LoggerFactory.getLogger(InboundOutboundTaskDemo.class);
 
     public static void main() {
         platformThreadsDemo();
+        virtualThreadsDemo();
     }
 
     private static void platformThreadsDemo() {
@@ -21,4 +29,33 @@ public class InboundOutboundTaskDemo {
             thread.start();
         }
     }
+
+    /**
+     * demonstrate how to start a virtual thread and how to use the latch to make it await
+     */
+    private static void virtualThreadsDemo() {
+        /*
+         * since virtual threads are daemon threads by default, we need to use countdown latch to make them wait
+         * in order to simulate the blocking scenario
+         */
+        CountDownLatch latch = new CountDownLatch(MAX_VIRTUAL_THREADS);
+        var virtualThreadBuilder = Thread.ofVirtual().name("VirtualThread - ", 1);
+        for (int i = 0; i < MAX_VIRTUAL_THREADS; i++) {
+            final Integer taskNumber = i;
+            Thread thread = virtualThreadBuilder.unstarted(() -> {
+                try {
+                    Task.doBlockingTask(taskNumber);
+                } finally {
+                    latch.countDown(); // Decrement the latch
+                }
+            });
+            thread.start();
+        }
+        try {
+            latch.await(); // Wait until all threads finish
+        } catch (InterruptedException e) {
+            logger.error("Count down latch was interrupted, details: {}", e.getMessage());
+        }
+    }
+
 }
