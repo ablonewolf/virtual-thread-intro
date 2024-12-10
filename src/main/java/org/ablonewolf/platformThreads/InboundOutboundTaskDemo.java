cf2b964412c3a1e37809a97b85f5612ad789abe6
Arka Bhuiyan <belmont66@duck.com>
2024-12-10 13:41:42 +0600

    Adopted the ThreadBuilder approach to create new platform threads.

--- src/main/java/org/ablonewolf/platformThreads/InboundOutboundTaskDemo.java
+++ src/main/java/org/ablonewolf/platformThreads/InboundOutboundTaskDemo.java
@@ -9,6 +9,7 @@ public class InboundOutboundTaskDemo {
     }
 
     private static void platformThreadsDemo() {
+        var threadBuilder = Thread.ofPlatform().name("PlatformThread - ", 1);
         for (int i = 0; i < MAX_PLATFORM_THREADS; i++) {
             final Integer taskNumber = i;
             /*
@@ -16,7 +17,7 @@ public class InboundOutboundTaskDemo {
              since we are exploring a concept, we are sticking to this approach for the time being
              due to the high value store in MAX_PLATFORM_THREADS, this code won't create that many threads
             */
-            Thread thread = new Thread(() -> Task.doBlockingTask(taskNumber));
+            Thread thread = threadBuilder.unstarted(() -> Task.doBlockingTask(taskNumber));
             thread.start();
         }
     }
