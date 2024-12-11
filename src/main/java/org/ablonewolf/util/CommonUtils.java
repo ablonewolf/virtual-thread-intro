c67bb3af206c8ed9d43c1acb6f7540b85f0d319c
Arka Bhuiyan <belmont66@duck.com>
2024-12-11 17:03:24 +0600

    Created a final util class named CommonUtils to hold common util operations; made it final and its constructor private so that it can neither be inherited nor be instantiated.

---
+++ src/main/java/org/ablonewolf/util/CommonUtils.java
@@ -0,0 +1,12 @@
+package org.ablonewolf.util;
+
+public final class CommonUtils {
+    private CommonUtils() {
+    }
+    public static Long timer(Runnable runnable) {
+        var startTime = System.currentTimeMillis();
+        runnable.run();
+        var endTime = System.currentTimeMillis();
+        return endTime - startTime;
+    }
+}
