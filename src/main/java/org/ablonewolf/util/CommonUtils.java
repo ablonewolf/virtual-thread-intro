43e03e8fa8a198cf51dcb579d018c710897b6897
Arka Bhuiyan <belmont66@duck.com>
2024-12-13 12:58:49 +0600

    Added a method to execute tasks by the passed Executor Service that will be used throughout the project.

--- src/main/java/org/ablonewolf/util/CommonUtils.java
+++ src/main/java/org/ablonewolf/util/CommonUtils.java
@@ -1,6 +1,12 @@
 package org.ablonewolf.util;
 
+import org.slf4j.Logger;
+
+import java.time.Duration;
+import java.util.concurrent.ExecutorService;
+
 public final class CommonUtils {
+
     private CommonUtils() {
     }
 
@@ -9,5 +15,15 @@ public final class CommonUtils {
         runnable.run();
         var endTime = System.currentTimeMillis();
         return endTime - startTime;
+    }
+
+    public static void executeByExecutorService(ExecutorService executorService, int taskCount, Logger logger) {
+        try (executorService) {
+            for (int i = 1; i <= taskCount; i++) {
+                int taskNumber = i;
+                executorService.submit(() -> ThreadUtils.executeIOTask(taskNumber, Duration.ofSeconds(5), logger));
+                logger.info("Task {} submitted", taskNumber);
+            }
+        }
     }
 }
