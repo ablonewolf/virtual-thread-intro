0829fa8e862c6ccd891766411dfbe22829506a48
Arka Bhuiyan <belmont66@duck.com>
2024-12-14 14:21:56 +0600

    added a class named API constants which hold the API endpoints for the external service as constant values.

---
+++ src/main/java/org/ablonewolf/externalService/APIConstants.java
@@ -0,0 +1,13 @@
+package org.ablonewolf.externalService;
+
+public final class APIConstants {
+
+    private APIConstants() {
+
+    }
+
+    // All API formats
+
+    public static final String PRODUCT_REQUEST_FORMAT = "http://localhost:7070/sec01/product/%d";
+    public static final String RATING_REQUEST_FORMAT = "http://localhost:7070/sec01/rating/%d";
+}
