6d5f18693b6b1a53284b8e8bf63d29c7f7993e16
Arka Bhuiyan <belmont66@duck.com>
2024-12-14 15:04:30 +0600

    compared the whole process between the usage of platform threads and virtual threads - in both cases, we used the concerned executor service.

--- src/main/java/org/ablonewolf/externalService/AccessResponseUsingFutureDemo.java
+++ src/main/java/org/ablonewolf/externalService/AccessResponseUsingFutureDemo.java
@@ -1,49 +1,53 @@
 package org.ablonewolf.externalService;
 
+import org.ablonewolf.util.CommonUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.ThreadFactory;
 
 public class AccessResponseUsingFutureDemo {
 
     private static final Logger logger = LoggerFactory.getLogger(AccessResponseUsingFutureDemo.class);
 
     void main() {
+
+        ThreadFactory threadFactory = Thread.ofPlatform().name("Platform thread - ", 1).factory();
+        try (var executor = Executors.newThreadPerTaskExecutor(threadFactory)) {
+            // this will be blocking since we are using platform threads.
+            var totalTime = CommonUtils.timer(() -> fetchProductFromAPI(executor));
+            logger.info("total time using cached threads: {}", totalTime);
+            executor.shutdown();
+        }
+
         try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+            // this will be non-blocking since we are using the virtual threads per task
+            var totalTime = CommonUtils.timer(() -> fetchProductFromAPI(executor));
+            logger.info("total time using virtual threads: {}", totalTime);
+            executor.shutdown();
+        }
+    }
 
-            try {
-                /*
-                 though the future.get() is blocking, internally JVM will place the virtual thread
-                 to its heap memory when the blocking network call takes place,
-                 and the OS will use its non-blocking API to fetch the desired data
-                 after that, the concerned virtual thread will be placed inside the concerned platform thread
-                 that's how the operation is non-blocking
-                */
-                List<Future<String>> products = new ArrayList<>();
-
-                /*
-                 * we are using separate for loops for the following reason
-                 * we are logging the product in another for loop to show that the API calling is not blocking
-                 * any of the platform threads; neither the get method of the future objects,
-                 * so all the following sequential API calls happen concurrently
-                 */
-                for (int i = 1; i <= 50; i++) {
-                    final var productId = i;
-                    products.add(executor.submit(() -> Client.getProduct(productId)));
-                }
-                for (var productInfo : products) {
-                    logger.info("Product name: {}", productInfo.get());
-                }
-
-            } catch (InterruptedException | ExecutionException e) {
-                logger.error("An error occurred while fetching the product info from the future object, details: {}",
-                    e.getMessage());
+    private void fetchProductFromAPI(ExecutorService executor) {
+        try {
+            List<Future<String>> products = new ArrayList<>();
+            for (int i = 1; i <= 50; i++) {
+                final var productId = i;
+                products.add(executor.submit(() -> Client.getProduct(productId)));
             }
+            for (var productInfo : products) {
+                logger.info("Product name: {}", productInfo.get());
+            }
+
+        } catch (InterruptedException | ExecutionException e) {
+            logger.error("An error occurred while fetching the product info from the future object, details: {}",
+                e.getMessage());
         }
     }
 }
