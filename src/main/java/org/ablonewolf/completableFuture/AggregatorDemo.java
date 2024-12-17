d48e4123110c344e7c9e3727e33a611864330533
Arka Bhuiyan <belmont66@duck.com>
2024-12-17 19:07:16 +0600

    made the product fetching sequential as well as non-blocking with the help of the allOf method of CompletableFuture.

--- src/main/java/org/ablonewolf/completableFuture/AggregatorDemo.java
+++ src/main/java/org/ablonewolf/completableFuture/AggregatorDemo.java
@@ -1,10 +1,14 @@
 package org.ablonewolf.completableFuture;
 
+import org.ablonewolf.models.ProductDTO;
 import org.ablonewolf.util.ThreadUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executors;
 
 public class AggregatorDemo {
@@ -17,12 +21,24 @@ public class AggregatorDemo {
 
         try (executorService) {
             AggregatorService aggregatorService = new AggregatorService(executorService);
+            List<CompletableFuture<ProductDTO>> futures = new ArrayList<>();
 
             for (int i = 1; i <= 50; i++) {
-                aggregatorService.getProductInfo(i)
-                    .thenAccept(product -> logger.info("Product: {}", product));
+                futures.add(aggregatorService.getProductInfo(i));
             }
+
+            CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]))
+                .thenRun(() -> {
+                    futures.forEach(future ->
+                        future.thenAccept(product -> logger.info("Product: {}", product)));
+                    logger.info("All products have been fetched.");
+                })
+                .exceptionally(ex -> {
+                    logger.error("An error occurred while fetching products, details: {}", ex.getMessage());
+                    return null;
+                });
         }
+
 
         ThreadUtils.sleep(Duration.ofSeconds(5));
     }
