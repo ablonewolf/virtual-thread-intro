9c2ccb7fc54c7a7ab43e623bd3439bc30f5158bb
Arka Bhuiyan <belmont66@duck.com>
2024-12-30 19:48:56 +0600

    submitted the task of fetching product and then printing its info to the executor service for performance boost.

--- src/main/java/org/ablonewolf/completableFuture/AggregatorDemo.java
+++ src/main/java/org/ablonewolf/completableFuture/AggregatorDemo.java
@@ -1,14 +1,10 @@
 package org.ablonewolf.completableFuture;
 
-import org.ablonewolf.models.ProductDTO;
 import org.ablonewolf.util.ThreadUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executors;
 
 public class AggregatorDemo {
@@ -19,27 +15,41 @@ public class AggregatorDemo {
         var virtualThreadFactory = Thread.ofVirtual().name("Virtual Thread - ", 1).factory();
         var executorService = Executors.newThreadPerTaskExecutor(virtualThreadFactory);
 
+        var startTime = System.currentTimeMillis();
         try (executorService) {
             AggregatorService aggregatorService = new AggregatorService(executorService);
-            List<CompletableFuture<ProductDTO>> futures = new ArrayList<>();
 
-            for (int i = 1; i <= 50; i++) {
-                futures.add(aggregatorService.getProductInfo(i));
+            for (int i = 1; i <= 500000; i++) {
+                final int index = i;
+                executorService.submit(() -> fetchAndPrintProductInfo(index, aggregatorService));
             }
 
-            CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]))
-                .thenRun(() -> {
-                    futures.forEach(future ->
-                        future.thenAccept(product -> logger.info("Product: {}", product)));
-                    logger.info("All products have been fetched.");
-                })
-                .exceptionally(ex -> {
-                    logger.error("An error occurred while fetching products, details: {}", ex.getMessage());
-                    return null;
-                });
         }
+        // we will prefer this approach when we need to fetch each product in an order
+//        List<CompletableFuture<ProductDTO>> productFutures = new ArrayList<>();
+//        CompletableFuture.allOf(productFutures.toArray(new CompletableFuture[0]))
+//            .thenRun(() -> {
+//                productFutures.forEach(future ->
+//                    future.thenAccept(product -> logger.info("Product: {}", product)));
+//                logger.info("All products have been fetched.");
+//            })
+//            .exceptionally(ex -> {
+//                logger.error("An error occurred while fetching products, details: {}", ex.getMessage());
+//                return null;
+//            });
 
+        var endTime = System.currentTimeMillis();
+        logger.info("Elapsed time: {} ms", endTime - startTime);
 
         ThreadUtils.sleep(Duration.ofSeconds(5));
+    }
+
+    private void fetchAndPrintProductInfo(Integer productId, AggregatorService aggregatorService) {
+        var productFuture = aggregatorService.getProductInfo(productId);
+        productFuture.thenAccept(productDTO -> logger.info("Product: {}", productDTO))
+            .exceptionally(ex -> {
+                logger.error("error occurred while printing product info, details: {}", ex.getMessage());
+                return null;
+            });
     }
 }
