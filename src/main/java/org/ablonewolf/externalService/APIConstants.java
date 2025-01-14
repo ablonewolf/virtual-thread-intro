448af083a45f4eb18429c6955a2f9d867a3d5d1e
Arka Bhuiyan <belmont66@duck.com>
2025-01-14 16:35:20 +0600

    placed a new back-end service and updated the API constants.

--- src/main/java/org/ablonewolf/externalService/APIConstants.java
+++ src/main/java/org/ablonewolf/externalService/APIConstants.java
@@ -5,9 +5,10 @@ public final class APIConstants {
     private APIConstants() {
 
     }
-
     // All API formats
 
-    public static final String PRODUCT_REQUEST_FORMAT = "http://localhost:7070/productApp/product/%d";
-    public static final String RATING_REQUEST_FORMAT = "http://localhost:7070/productApp/rating/%d";
+    public static final String PRODUCT_REQUEST_FORMAT = "http://localhost:7070/productApp/product/%s";
+    public static final String RATING_REQUEST_FORMAT = "http://localhost:7070/productApp/rating/%s";
+    public static final String PRODUCT_STREAMING_REQUEST_FORMAT = "http://localhost:7070/productApp/stream/products/%s";
+    public static final String RATING_STREAMING_REQUEST_FORMAT = "http://localhost:7070/productApp/stream/ratings/%s";
 }
