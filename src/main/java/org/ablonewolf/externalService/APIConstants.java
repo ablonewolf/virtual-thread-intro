19171050c685173bfb682141263c92e9e6e69ecf
Arka Bhuiyan <belmont66@duck.com>
2025-01-14 11:36:52 +0600

    added a new packaged backend application and used that as an external service and adjusted the API constants accordingly.

--- src/main/java/org/ablonewolf/externalService/APIConstants.java
+++ src/main/java/org/ablonewolf/externalService/APIConstants.java
@@ -8,6 +8,6 @@ public final class APIConstants {
 
     // All API formats
 
-    public static final String PRODUCT_REQUEST_FORMAT = "http://localhost:7070/sec01/product/%d";
-    public static final String RATING_REQUEST_FORMAT = "http://localhost:7070/sec01/rating/%d";
+    public static final String PRODUCT_REQUEST_FORMAT = "http://localhost:7070/productApp/product/%d";
+    public static final String RATING_REQUEST_FORMAT = "http://localhost:7070/productApp/rating/%d";
 }
