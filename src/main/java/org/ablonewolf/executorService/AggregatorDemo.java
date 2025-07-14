47ee97099ec5f6b33b56c690854c0e49003e9a7b
Arka Bhuiyan <belmont66@duck.com>
2025-07-14 16:41:59 +0600

    used a simple for loop instead of for each and list since we are eagerly fetching the product from the aggregator service; included a platform thread to check in comparison.

--- src/main/java/org/ablonewolf/executorService/AggregatorDemo.java
+++ src/main/java/org/ablonewolf/executorService/AggregatorDemo.java
@@ -1,43 +1,31 @@
 package org.ablonewolf.executorService;
 
-import org.ablonewolf.models.ProductDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.stream.IntStream;
 
 public class AggregatorDemo {
 
     private static final Logger logger = LoggerFactory.getLogger(AggregatorDemo.class);
 
+    @SuppressWarnings("unused")
     void main() {
-        try (var executorService = Executors.newVirtualThreadPerTaskExecutor()) {
-            var aggregatorService = new AggregatorService(executorService);
+        var platformThreadFactory = Thread.ofPlatform().name("Platform Thread - ", 1).factory();
+        var virtualThreadFactory = Thread.ofVirtual().name("Virtual Thread - ", 1).factory();
+        var executorService = Executors.newThreadPerTaskExecutor(platformThreadFactory);
+        var aggregatorService = new AggregatorService(executorService);
 
-            var productFutures = IntStream.rangeClosed(1, 50)
-                .mapToObj(id -> executorService.submit(() -> aggregatorService.getProductInfo(id)))
-                .toList();
+        var startTime = System.currentTimeMillis();
 
-            var productList = productFutures.stream()
-                .map(AggregatorDemo::toProductDTO)
-                .toList();
-
-            for (var product : productList) {
-                logger.info("Product info: {}", product);
+        for (int i = 1; i <= 500000; i++) {
+            var productInfo = aggregatorService.getProductInfo(i);
+            logger.info("product info: {}", productInfo);
             }
 
-        }
-    }
+        var endTime = System.currentTimeMillis();
+        logger.info("Elapsed time: {} ms", endTime - startTime);
 
-    private static ProductDTO toProductDTO(Future<ProductDTO> productFuture) {
-        try {
-            return productFuture.get();
-        } catch (InterruptedException | ExecutionException e) {
-            logger.error("Error occurred while retrieving product info from the future, details: {}", e.getMessage());
-        }
-        return null;
+
     }
 }
