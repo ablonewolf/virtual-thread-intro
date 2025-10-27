43ed8fc3e10f2a2403dfd60096f1b59361b55dc9
Arka Bhuiyan <belmont66@duck.com>
2025-10-27 15:52:02 +0600

    added the alternative way for fetching products sequentially over the API call for comparison purpose.

--- src/main/java/org/ablonewolf/executorService/DemonstrateMapConcurrent.java
+++ src/main/java/org/ablonewolf/executorService/DemonstrateMapConcurrent.java
@@ -1,9 +1,12 @@
 package org.ablonewolf.executorService;
 
+import org.ablonewolf.models.ProductDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.stream.Gatherers;
 import java.util.stream.IntStream;
@@ -19,10 +22,17 @@ public class DemonstrateMapConcurrent {
 
 		LocalDateTime startTime = LocalDateTime.now();
 
-		var productList = IntStream.rangeClosed(1, 500000)
+		List<ProductDTO> productList = new ArrayList<>();
+
+		productList = IntStream.rangeClosed(1, 500000)
 				.boxed()
 				.gather(Gatherers.mapConcurrent(8, aggregatorService::getProductInfo))
 				.toList();
+
+		/*for (int i = 1; i <= 500000; i++) {
+			var productInfo = aggregatorService.getProductInfo(i);
+			productList.add(productInfo);
+		}*/
 
 		LocalDateTime endTime = LocalDateTime.now();
 
